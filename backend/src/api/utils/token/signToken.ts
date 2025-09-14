44bad70a1cf943388f7f5208d17be2afe8ba7c88
Nhu Khanh <21522204@gm.uit.edu.vn>
2025-09-14 14:44:09 +0700

    fix deploy err

--- backend/src/api/utils/token/signToken.ts
+++ backend/src/api/utils/token/signToken.ts
@@ -1,14 +1,33 @@
 /** @format */
 
-import jwt from "jsonwebtoken";
+import jwt, { SignOptions } from "jsonwebtoken";
 
-const signToken = (
-  payload: unknown,
-  expiresIn = process.env.JWT_EXPIRES_IN
-) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: expiresIn,
-  });
-};
+function signToken<T extends object>(
+  payload: T,
+  expiresIn: string | number = process.env.JWT_EXPIRES_IN || "1d"
+): string {
+  return jwt.sign(
+    payload,
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn,
+    } as SignOptions
+  );
+}
 
 export default signToken;
+
+// import jwt, { SignOptions } from "jsonwebtoken";
+
+// export function signToken<T extends object>(
+//   payload: T,
+//   expiresIn: string | number = process.env.JWT_EXPIRES_IN || "1d",
+//   options: SignOptions = {}
+// ): string {
+//   return jwt.sign(payload, process.env.JWT_SECRET as string, {
+//     expiresIn,
+//     ...options,
+//   });
+// }
+
+// export default signToken;
